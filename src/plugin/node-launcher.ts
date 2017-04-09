e5f39e743cc4745e4dbd2c1e983648bb47622f2c
Williams Medina <williams.medinaa@gmail.com>
2017-04-09 00:16:03 -0400

    fix: do not parse unable sourcemaps

--- src/plugin/node-launcher.ts
+++ src/plugin/node-launcher.ts
@@ -1,5 +1,6 @@
 import { ChromeDebuggingProtocolLauncher } from 'atom-bugs-chrome-debugger/lib/launcher'
-import { dirname } from 'path'
+import { dirname, join } from 'path'
+import { get, extend } from 'lodash'
 
 export class NodeLauncher extends ChromeDebuggingProtocolLauncher {
   public binaryPath: string
@@ -11,18 +12,32 @@ export class NodeLauncher extends ChromeDebuggingProtocolLauncher {
     return dir.replace(/^~/, process.env.HOME)
   }
   getLauncherArguments () {
-    return [
+    let a = [
       `--inspect`,
       `--debug-brk=${this.portNumber}`,
       this.normalizePath(this.scriptPath)
-    ].concat(this.launchArguments)
+    ]
+    .concat(this.launchArguments)
+    return a
   }
   getProcessOptions () {
+    let projectPath = this.cwd || this.normalizePath(dirname(this.scriptPath))
+    let envPath = get(process, 'env.PATH')
+    let npmPath = join(projectPath, 'node_modules', '.bin')
     return {
       detached: true,
       shell: true,
-      cwd: this.cwd || this.normalizePath(dirname(this.scriptPath)),
-      env: this.environmentVariables
+      cwd: projectPath,
+      env: extend({
+        SHELL: get(process, 'env.SHELL'),
+        TERM: get(process, 'env.TERM'),
+        TMPDIR: get(process, 'env.TMPDIR'),
+        USER: get(process, 'env.USER'),
+        PATH: `${npmPath}:${envPath}`,
+        PWD: get(process, 'env.PWD'),
+        LANG: get(process, 'env.LANG'),
+        HOME: get(process, 'env.HOME')
+      }, this.environmentVariables)
     }
   }
   getBinaryPath (): string {
